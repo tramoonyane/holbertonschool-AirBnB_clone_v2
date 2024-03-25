ec2b1fa0d8bd80148bb42f8c5d944fa8b802c9c8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-26 00:56:06 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,20 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.engine.file_storage import FileStorage
 
+
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        self.storage = FileStorage()
+
+
+if __name__ == '__main__':
+    unittest.main()
