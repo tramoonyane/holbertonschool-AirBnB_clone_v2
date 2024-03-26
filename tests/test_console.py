a495163dca1d8f82d0bb6a179e1fa88dfeaf1346
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-26 02:34:20 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,9 +1,6 @@
 import unittest
 import sys
-sys.path.append(
-    'holbertonschool-AirBnB_clone_v2/models
-/engine/'
-)
+sys.path.append('holbertonschool-AirBnB_clone_v2/models/engine/')
 import os
 from unittest.mock import patch
 from io import StringIO
