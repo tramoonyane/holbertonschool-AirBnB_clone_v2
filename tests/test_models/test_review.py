7fe4927abea337298fb77b595feb2066e0e59f03
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-27 09:06:20 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -11,14 +11,4 @@ class test_review(test_basemodel):
         """ """
         super().__init__(*args, **kwargs)
         self.name = "Review"
-        self.value = Review
-
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
-
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.value = Review 
