3c82e0c2f849c5c1302e8f9941bd4562128cd139
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-27 09:04:53 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -18,11 +18,6 @@ class test_review(test_basemodel):
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
     def test_text(self):
         """ """
         new = self.value()
