df1d58cd9954f0fda25fa488f4843f3cf8b9faa0
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-27 18:20:24 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+"""
+initialize the models package
+"""
+
 from os import getenv
 
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
+# DBSTORAGE
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
+# FILESTORAGE
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
