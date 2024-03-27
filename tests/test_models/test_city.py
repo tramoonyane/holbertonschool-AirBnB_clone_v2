10f57c7f20ebf181b36e8d1d3c29d151721d5f96
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-27 09:22:48 +0200

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -15,11 +15,6 @@ class test_City(test_basemodel):
         self.name = "City"
         self.value = City
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
-
 
 class Test_PEP8(unittest.TestCase):
     """test User"""
