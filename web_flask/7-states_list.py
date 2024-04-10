54d006bf720319becbe62eaeecb010c736cd91ab
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-10 23:57:16 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,27 @@
+#!/usr/bin/python3
+"""
+flask model
+"""
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_data(self):
+    """
+        refrech data
+    """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all("State").values()
-    states = sorted(states, key=lambda state: state.name)
+    """ return all states in the db  """
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
