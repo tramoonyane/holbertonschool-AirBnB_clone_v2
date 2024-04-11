54fcb51994905a1355248c3285880c64f4c8df19
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-11 02:22:17 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,6 +12,12 @@ def states_list():
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
