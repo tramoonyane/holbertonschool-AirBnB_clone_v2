d8802196f0c65f601c2012f4dd079d1e8a88edb7
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-12 13:38:35 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Simple module that starts a Flask web application
+Starting to display formatted text and conditional message
+Addind basic template
 """
-
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def handle_appcontext(self):
+    """
+    Method for handling app context
+    """
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display all State objects in alphabetical order"""
+def list_states():
+    """
+    Displays 7-states_list html page
+    """
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    """ app listening on host 0.0.0.0 and port 5000 """
+    app.run(host='0.0.0.0', port='5000')
